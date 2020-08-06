946a08188f32ecad67340e0b392f313400a824f5
knair410 <Krishnan_Nair@comcast.com>
2020-08-06 17:23:45 +0530

    Add getter function to fetch Reboot reason

--- source/WebConfig/webcfg_internal.c
+++ source/WebConfig/webcfg_internal.c
@@ -24,6 +24,7 @@
 #define MODEL_NAME		     "Device.DeviceInfo.ModelName"
 #define PRODUCT_CLASS		     "Device.DeviceInfo.ProductClass"
 #define CONN_CLIENT_PARAM	     "Device.NotifyComponent.X_RDKCENTRAL-COM_Connected-Client"
+#define LastREBOOT_REASON            "Device.DeviceInfo.X_RDKCENTRAL-COM_LastRebootReason"
 /*----------------------------------------------------------------------------*/
 /*                               Data Structures                              */
 /*----------------------------------------------------------------------------*/
@@ -76,4 +77,11 @@ char * getFirmwareVersion()
 char * getConnClientParamName()
 {
 	return CONN_CLIENT_PARAM;
+}
+
+char * getreboot_reason()
+{
+	char *reboot_reason = NULL;
+	reboot_reason = getParameterValue(LastREBOOT_REASON);
+	return reboot_reason;
 }
