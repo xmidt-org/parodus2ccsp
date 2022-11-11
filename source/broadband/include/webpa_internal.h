c7618c01597d3198bf42c6281b31bf8c9b6b4b44
rchromatic <rchromatic@gmail.com>
2022-11-11 17:07:55 -0500

    Update webpa_internal.h
    Added DEVICE_MAC special handling  for XLE

--- source/broadband/include/webpa_internal.h
+++ source/broadband/include/webpa_internal.h
@@ -69,7 +69,7 @@
 #define DEVICE_MAC                   "Device.Ethernet.Interface.5.MACAddress"
 #elif defined(RDKB_EMU)
 #define DEVICE_MAC                   "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"
-#elif defined(_HUB4_PRODUCT_REQ_)
+#elif defined(_HUB4_PRODUCT_REQ_) || defined(_WNXL11BWL_PRODUCT_REQ_)
 #define DEVICE_MAC                   "Device.DeviceInfo.X_COMCAST-COM_WAN_MAC"
 #else
 #define DEVICE_MAC                   "Device.X_CISCO_COM_CableModem.MACAddress"
