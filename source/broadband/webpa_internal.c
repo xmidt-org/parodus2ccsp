98390d006823fbe4307b2d405bb8fe7609c20393
keerthana.p <keerthana_pandurangan@comcast.com>
2024-08-13 13:29:53 +0000

    RDKBACCL-393 : BPI flagname correction

--- source/broadband/webpa_internal.c
+++ source/broadband/webpa_internal.c
@@ -199,7 +199,7 @@ int waitForOperationalReadyCondition()
 	{
 		return EPON_FAILED;
 	}
-#elif !defined(PLATFORM_RASPBERRYPI) && !defined(RDKB_EMU) && !defined(PLATFORM_BANANAPI_R4)
+#elif !defined(PLATFORM_RASPBERRYPI) && !defined(RDKB_EMU) && !defined(_PLATFORM_BANANAPI_R4_)
     if(check_ethernet_wan_status() != WDMP_SUCCESS)
 	{
 #if !defined(_SKY_HUB_COMMON_PRODUCT_REQ_)
