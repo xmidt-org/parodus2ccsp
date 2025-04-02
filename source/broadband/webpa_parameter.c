e20f463d8143ff0f4e2130b542d17766fe68b86e
venkata krishna vanga <venkatakrishna.vanga@cognizant.com>
2025-04-02 08:58:55 -0400

    Update webpa_parameter.c
    Uncommented the changes.

--- source/broadband/webpa_parameter.c
+++ source/broadband/webpa_parameter.c
@@ -83,11 +83,11 @@ void getValues(const char *paramName[], const unsigned int paramCount, int index
         {
             break;
         }
-       /* if ((paramCount == 1) && (parameterName[(strlen(parameterName)-1)] == '.'))
+        if ((paramCount == 1) && (parameterName[(strlen(parameterName)-1)] == '.'))
         {
             count = 1;
             isLargeWildCard = 0;
-        } */
+        }
         WalPrint("parameterName: %s count: %d\n",parameterName,count);
         for(i = 0; i < count; i++)
         {
@@ -101,7 +101,6 @@ void getValues(const char *paramName[], const unsigned int paramCount, int index
 
     if(error != 1)
     {
-	isLargeWildCard = 0;
         WalPrint("compCount : %d paramCount: %d\n",compCount,paramCount);
         if(compCount > paramCount)
         {
